a314e3b0502bd9115d7286bfc13c9e970ba8fbf5
rahulemve-lang <rahulemve@gmail.com>
2026-09-05 04:08:03 +0530

    feat(models/request.js): add line 15/20

--- models/request.js
+++ models/request.js
@@ -12,3 +12,4 @@ const RequestSchema = new mongoose.Schema({
   location: {
     type: { type: String, enum: ['Point'], default: 'Point' },
     coordinates: { type: [Number], required: true }
+  },
