4e703e1c6b81513f6a5355c4c4ca40adcc99e09d
rahulemve-lang <rahulemve@gmail.com>
2026-09-05 04:06:31 +0530

    feat(models/rescuer.js): add line 6/17

--- models/rescuer.js
+++ models/rescuer.js
@@ -3,3 +3,4 @@ const mongoose = require('mongoose');
 const RescuerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   mobile: { type: String, required: true },
+  capability: { type: String, required: true },
